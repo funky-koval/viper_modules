a942d0d5ff70990327d63bad5d16c6d2bb983df7
funky-koval <103317117+funky-koval@users.noreply.github.com>
2025-03-11 15:38:18 +0000

    Create box_js_test_obf001.js

---
+++ box_js_test_obf001.js
@@ -0,0 +1,16 @@
+// Basic obfuscated JavaScript sample for BoxJS testing
+(function(){
+    var _0xabc1 = ['\x48\x65\x6c\x6c\x6f', '\x42\x6f\x78\x4a\x53'];
+    (function(_0x1a2b, _0x3c4d) {
+        _0x1a2b[1] = _0x1a2b[1] + "!";
+    })(_0xabc1);
+    console.log(_0xabc1[0] + ", " + _0xabc1[1]);
+})();
+
+// Another eval-based obfuscation technique
+var encodedString = btoa("This is a hidden message!");
+eval("console.log(atob('" + encodedString + "'));");
+
+// URL Extraction Test
+var suspicious_url = "http:\/\/evil-site.com";
+console.log("Contact: " + suspicious_url);
