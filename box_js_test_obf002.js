e7eb12e9699801471535cc6d4e2525abb139d6a5
funky-koval <103317117+funky-koval@users.noreply.github.com>
2025-03-11 15:41:12 +0000

    Create box_js_test_obf002.js

---
+++ box_js_test_obf002.js
@@ -0,0 +1,8 @@
+// Simple obfuscation using array substitution
+(function() {
+    var _0x1234 = ['\x48\x65\x6c\x6c\x6f', '\x42\x6f\x78\x4a\x53'];
+    console.log(_0x1234[0] + ", " + _0x1234[1]);
+})();
+
+// Basic eval obfuscation
+eval(String.fromCharCode(99, 111, 110, 115, 111, 108, 101, 46, 108, 111, 103, 40, 34, 84, 101, 115, 116, 32, 83, 116, 114, 105, 110, 103, 34, 41));
